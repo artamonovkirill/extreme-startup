e9546059b2f64caa08a963c438519095b82d5850
Kirill Artamonov <artamonov.kirill@gmail.com>
2019-01-15 16:01:01 +0100

    feat: haskell logging

--- haskell/Startup.hs
+++ haskell/Startup.hs
@@ -1,10 +1,12 @@
 module Main where
 
-import Happstack.Server (nullConf, logMAccess, simpleHTTP, toResponse, ok, look, ServerPart, queryString)
+import Happstack.Server (nullConf, simpleHTTP, ok, look, ServerPart, queryString)
+import Control.Monad.Trans (liftIO)
 
 query :: ServerPart String
 query =
     do q <- queryString $ look "q"
+       liftIO $ putStrLn $ "Query is: " ++ q
        ok $ respond $ q
 
 respond :: String -> String
