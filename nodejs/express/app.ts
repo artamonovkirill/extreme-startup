ff49c6fc60eb30edac2be20a55e152a16440ad8c
Kirill Artamonov <artamonov.kirill@gmail.com>
2021-05-24 21:32:18 +0200

    Add missing dependencies and imports

--- nodejs/express/app.ts
+++ nodejs/express/app.ts
@@ -1,8 +1,9 @@
 import * as express from 'express';
+import {Request, Response} from 'express';
 
 export const app = express();
 
-app.get('/', (request, response) => {
+app.get('/', (request: Request, response: Response) => {
     let query = request.query.q;
     console.log(`Query is: ${query}`);
     response.send(query || 'Hello!');
