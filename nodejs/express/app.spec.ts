c17ace9b9ac0e4ccc7fcb81f87191776ef500ec8
Kirill Artamonov <artamonov.kirill@gmail.com>
2026-02-28 11:38:52 +0100

    :heavy_minus_sign: reduce dependencies
    - replace mocha with node:test
    - replace ts-node (stale) with tsx

--- nodejs/express/app.spec.ts
+++ nodejs/express/app.spec.ts
@@ -1,18 +1,17 @@
-import * as request from 'supertest';
+import { describe, it } from 'node:test';
+import request from 'supertest';
 
 import {app} from "./app";
 
 describe('App', () => {
-    it('exposes root endpoint', done => {
-        request(app)
+    it('exposes root endpoint', async () => {
+        await request(app)
             .get('/')
-            .expect('Hello!')
-            .end(done);
+            .expect('Hello!');
     });
-    it('returns query if given', done => {
-        request(app)
+    it('returns query if given', async () => {
+        await request(app)
             .get('/?q=foo')
-            .expect('foo')
-            .end(done);
+            .expect('foo');
     });
 });
