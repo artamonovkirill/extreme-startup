bd12c1ff2b5dc7d088f36ff36e2ae15b50620383
Kirill Artamonov <artamonov.kirill@gmail.com>
2026-03-28 10:57:18 +0100

    :wrench: Fix routing for Javalin 7

--- java/javalin/src/main/java/com/github/artamonovkirill/ExtremeStartup.java
+++ java/javalin/src/main/java/com/github/artamonovkirill/ExtremeStartup.java
@@ -2,14 +2,19 @@ package com.github.artamonovkirill;
 
 import io.javalin.Javalin;
 
+import static io.javalin.apibuilder.ApiBuilder.*;
+
 public class ExtremeStartup {
     public static Javalin createApp() {
-        return Javalin.create()
-                .get("/", ctx -> {
+        return Javalin.create(config -> {
+            config.routes.apiBuilder(() -> {
+                get("/", ctx -> {
                     var query = ctx.queryParam("q");
                     System.out.printf("Query is: %s%n", query);
                     ctx.result(query);
                 });
+            });
+        });
     }
 
     public static void main(String... args) {
